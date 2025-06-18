140a9c5cedc5b60db8733988faa2f37a138dddeb
Iman-Alzadajli <imoimo033113@gmail.com>
2025-06-18 18:20:10 +0400

    donev3

---
+++ PAL.Shop/Controllers/XShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PAL.Shop.Controllers
+{
+    public class XShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
